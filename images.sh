b9e27af2485e97282e73e881326ee50e2336a7d5
Focardi François-Philippe <francois.focardi@hotmail.fr>
2022-11-18 19:37:12 +0100

    Ajouts de la fonctionnalite add folder

--- images.sh
+++ images.sh
@@ -3,23 +3,23 @@ RED_COLOR="\033[1;31m";
 BLUE_COLOR="\033[1;34m";
 GREEN_COLOR="\033[1;32m";
 NO_COLOR="\033[0m";
+ACCEPT_EXTENSION=("png" "jpg" "jpeg")
 help () {
     echo -e "${GREEN_COLOR}gensite images:${NO_COLOR}";
     echo;
     echo "Options:";
     echo -e "\t-a, --add    {image}     Add an image (jpg or png) to the project. If several images use space to separate images.";
-    echo -e "\t-f, --folder {path}      Add all images in a folder to the project.";
+    echo -e "\t-f, --folder {path}      Add all images (jpg or png) in a folder to the project.";
     echo;
 }
 
 add () {
-    extensionsAcceptees=("png" "jpg" "jpeg")
     for image in "${@:3}" # Iterate on images but not command args
     do
         if test -f $image;then
             filename=$(basename -- "$image")
             extension="${filename##*.}"
-            if [[ " ${extensionsAcceptees[*]} " =~ " ${extension} " ]]; then
+            if [[ " ${ACCEPT_EXTENSION[*]} " =~ " ${extension} " ]]; then
                 cp $image ./images
             else
                 echo "Le fichier ${image} doit etre de type png ou jpg"
@@ -30,7 +30,24 @@ add () {
         fi
     done
 }
-
+add_folder () {
+    for folder in "${@:3}" # Iterate on images but not command args
+    do
+        if test -d $folder;then
+            for file in $folder*; do
+                filename=$(basename -- "$file")
+                extension="${filename##*.}"
+                if [[ " ${ACCEPT_EXTENSION[*]} " =~ " ${extension} " ]]; then
+                    cp $file ./images
+                else
+                    echo "Le fichier ${file} n'est pas telecharge, il doit etre de type png ou jpg"
+                fi
+            done
+        else
+            echo "${folder} n'est pas un dossier ou n'existe pas"
+        fi
+    done
+}
 
 if [ $# -le 1 ]; # Not enouth params
     then
@@ -45,6 +62,9 @@ else
 
         "-a" | "--add")
             add $*;
+            ;;
+        "-f" | "--folder")
+            add_folder $*;
             ;;
             
 
